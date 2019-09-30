749b19fa8c64b33795386f17c4c9e67ec22b66d9
Alfian Pramudita <alfian.pramudita@gmail.com>
2019-09-30 23:37:22 +0700

    implements sendMessage on query

--- c1/src/__tests__/app.test.ts
+++ c1/src/__tests__/app.test.ts
@@ -1,8 +1,15 @@
 import request from 'supertest';
 import { app } from '../app';
 import path from 'path';
+import * as nats from '../generals/nats';
+
+jest.mock('../generals/nats');
 
 describe('query controller', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   it('should return success and size on proper request', async () => {
     const filePath = path.join(__dirname, '../../assets/github.png');
 
@@ -17,5 +24,18 @@ describe('query controller', () => {
     const response = await request(app).post('/query');
 
     expect(response.body).toMatchSnapshot();
+  });
+
+  it('should call sendMessage when called with valid parameters', async () => {
+    const filePath = path.join(__dirname, '../../assets/github.png');
+    const mockedSendMessage = nats.sendMessage as jest.Mock;
+    mockedSendMessage.mockImplementationOnce(() => 'GUID');
+
+    await request(app)
+      .post('/query')
+      .attach('image', filePath);
+
+    expect(mockedSendMessage).toHaveBeenCalledTimes(1);
+    expect(mockedSendMessage.mock.calls[0][0].originalname).toBe('github.png');
   });
 });
--- c1/src/app.ts
+++ c1/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import multer from 'multer';
+import { sendMessage } from './generals/nats';
 
 const app = express();
 const upload = multer();
 
-app.post('/query', upload.any(), (request, response) => {
+app.post('/query', upload.any(), async (request, response) => {
   if (!Array.isArray(request.files) || request.files.length === 0) {
     return response.status(400).send({
       error: {
@@ -16,6 +17,8 @@ app.post('/query', upload.any(), (request, response) => {
 
   const [firstImage] = request.files;
   const imageSize = firstImage.size / Math.pow(1024, 1);
+
+  await sendMessage(firstImage);
 
   return response.status(200).send({
     status: 'SUCCESS',
