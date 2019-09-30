b43cefef53feba3c9dcfa706713ef12e75516d32
Alfian Pramudita <alfian.pramudita@gmail.com>
2019-09-30 23:50:31 +0700

    send uploaded image size from c3 to c2

--- c3/src/generals/constants.ts
+++ c3/src/generals/constants.ts
@@ -2,7 +2,8 @@ import { v4 as uuid } from 'uuid';
 
 export const NATS_STREAMING_URL =
   process.env.NATS_STREAMING_URL || 'nats://localhost:4222';
-export const NATS_CHANNEL = 'image.uploaded';
+export const IMAGE_UPLOADED_CHANNEL = 'image.uploaded';
+export const IMAGE_PROCESSED_CHANNEL = 'image.processed';
 export const NATS_QUEUE_GROUP = 'image.uploaded.worker';
 export const NATS_CLUSTER = 'test-cluster';
 export const NATS_CLIENT_ID = uuid();
--- c3/src/listener.ts
+++ c3/src/listener.ts
@@ -1,20 +1,37 @@
 import { Message, Stan } from 'node-nats-streaming';
-import { NATS_CHANNEL, NATS_QUEUE_GROUP } from './generals/constants';
+import {
+  IMAGE_UPLOADED_CHANNEL,
+  NATS_QUEUE_GROUP,
+  IMAGE_PROCESSED_CHANNEL,
+} from './generals/constants';
 
 function runWorker(stan: Stan) {
   stan.on('connect', () => {
     const opts = stan.subscriptionOptions().setDurableName('image.processor');
-    const subscription = stan.subscribe(NATS_CHANNEL, NATS_QUEUE_GROUP, opts);
+    const subscription = stan.subscribe(
+      IMAGE_UPLOADED_CHANNEL,
+      NATS_QUEUE_GROUP,
+      opts,
+    );
 
     subscription.on('ready', () => {
       console.log('Subscription ready');
     });
 
     subscription.on('message', async (msg: Message) => {
+      console.log(`Incoming Message: ${msg.getSequence()}`);
       const data = msg.getData();
+      const image = JSON.parse(String(data));
+      const imageSize = image.size / Math.pow(1024, 1);
 
       try {
-        console.log(data);
+        stan.publish(
+          IMAGE_PROCESSED_CHANNEL,
+          JSON.stringify({ size: imageSize }),
+          (err) => {
+            console.log({ err }, 'Message Published');
+          },
+        );
       } catch (error) {
         console.error(error);
       }
